386f58e1d062ce169db23aabe293d8e861ac6ebd
Nan Davenport <nannigan@sonic.net>
2014-09-15 22:53:36 +0000

    intiial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catsplurty::Application.config.secret_key_base = '5985e3683a807dbde87872cc7ba13322677ff33a6135e689890d3ee3b48236dae906b1d4671eafc2aa35b5379e7cbb5b882cf01e0258ac0698a85ca7cbdeb6c6'
